33800cb825e0f4af83a7928e0805880861a2076f
Md. Latiful Kabir <latifulkabir@gmail.com>
2022-06-16 01:43:53 +0600

    final commit

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,5 @@
+document.getElementById('display-data').style.display='none';
+
 const API_KEY = `ce3302465c29ee6cfa30c1e3c799006f`;
 const searchTemp = () => {
     const city = document.getElementById('city-name').value;
@@ -10,12 +12,15 @@ const setInnerText = (id, text) => {
     document.getElementById(id).innerText = text;
 }
 const displayTemp = temp => {
-    console.log(temp);
+    document.getElementById('display-data').style.display='block';
     setInnerText('city', temp.name);
     setInnerText('temp', temp.main.temp);
     setInnerText('feels-temp', temp.main.feels_like);
     setInnerText('temp-status', temp.weather[0].main);
-    // console.log(temp.weather[0].icon);
+    setInnerText('sun-status', temp.sys.sunrise);
+    setInnerText('sun-status2', temp.sys.sunset);
+    setInnerText('humidity', temp.main.humidity);
+    setInnerText('pressure', temp.main.pressure);
     // set weather icon
     const url = `http://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`;
     const imgIcon = document.getElementById('weather-icon');
